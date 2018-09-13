e86c26d9c75f35569580a84a7b5c453ea83e563f
Antonio Gil <antonioluisgil@gmail.com>
2018-09-13 12:52:34 +0200

    Refactor views

--- views/index.view.php
+++ views/index.view.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Todos app</title>
-  <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body>
-  <div class="container mx-auto mt-4">
-    <h1>Todo List</h1>
+<?php require 'partials/head.php'; ?>
 
-    <ul class="list-reset">
-      <?php foreach($tasks as $task) : ?>
-        <li class="mt-3 p-2 bg-grey-lighter border border-grey-light rounded text-grey-darker shadow <?= $task->completed ? 'line-through text-grey' : '' ?>">
-          <?= $task->description; ?>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+<h1>Todo List</h1>
+
+<ul class="list-reset">
+  <?php foreach($tasks as $task) : ?>
+    <li class="mt-3 p-2 bg-grey-lighter border border-grey-light rounded text-grey-darker shadow <?= $task->completed ? 'line-through text-grey' : '' ?>">
+      <?= $task->description; ?>
+    </li>
+  <?php endforeach; ?>
+</ul>
   
-</body>
-</html>
+<?php require 'partials/footer.php'; ?>
---
+++ views/partials/nav.php
@@ -0,0 +1,14 @@
+<ul class="list-reset flex justify-end my-4">
+  <li class="ml-6">
+    <a class="text-blue hover:text-blue-darker no-underline" href="/">Home</a>
+  </li>
+  <li class="ml-6">
+    <a class="text-blue hover:text-blue-darker no-underline" href="/about">About us</a>
+  </li>
+  <li class="ml-6">
+    <a class="text-blue hover:text-blue-darker no-underline" href="/about/culture">About our culture</a>
+  </li>
+  <li class="ml-6">
+    <a class="text-blue hover:text-blue-darker no-underline" href="/contact">Contact</a>
+  </li>
+</ul>
